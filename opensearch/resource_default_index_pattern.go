da03ede488913c24983d31951b0f73dc4d176656
Max Opperman <max.opperman@moia.io>
2025-03-13 10:50:24 +0100

    fmt

--- opensearch/resource_default_index_pattern.go
+++ opensearch/resource_default_index_pattern.go
@@ -2,6 +2,7 @@ package opensearch
 
 import (
 	"context"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/rs/zerolog/log"
